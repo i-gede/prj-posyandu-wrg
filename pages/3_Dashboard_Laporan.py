fd534cb9d193f0817e46028af28b829ad066e2b3
i-gede <igededarma@yahoo.com>
2025-08-03 22:21:15 +0800

    update

--- pages/3_Dashboard_Laporan.py
+++ pages/3_Dashboard_Laporan.py
@@ -235,7 +235,7 @@ def page_dashboard():
 
                     st.markdown(
                         """
-                        <div style="display: flex; align-items: center; justify-content: center; height: 58px;">
+                        <div style="display: flex; align-items: center; justify-content: center; height: 29px;">
                         """,
                         unsafe_allow_html=True
                     )
