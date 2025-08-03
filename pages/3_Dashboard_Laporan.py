9d686acb1d7884dc6acf8b3f3e897ecaa5d11e65
i-gede <igededarma@yahoo.com>
2025-08-03 15:28:36 +0800

    update

--- pages/3_Dashboard_Laporan.py
+++ pages/3_Dashboard_Laporan.py
@@ -242,7 +242,7 @@ def page_dashboard():
                     st.markdown(f"""
                     <div style="background-color:#F0F2F6; padding:14px 18px; border-radius:10px; height: 95%; display: flex; flex-direction: row; justify-content: center;">
                         <strong>{label}</strong><br>
-                        👥 Total: <strong>{total}</strong> &nbsp;&nbsp;
+                        👥 Total: <strong>{total}</strong>;
                         👦 Laki-laki: <strong>{laki}</strong> &nbsp;&nbsp;
                         👧 Perempuan: <strong>{perempuan}</strong>
                     </div>
