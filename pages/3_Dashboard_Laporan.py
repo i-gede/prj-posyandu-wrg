a9faaba228faecf833d1e844e93256c174cfc4cb
i-gede <igededarma@yahoo.com>
2025-08-03 09:46:11 +0800

    update

--- pages/3_Dashboard_Laporan.py
+++ pages/3_Dashboard_Laporan.py
@@ -119,11 +119,9 @@ def page_dashboard():
         df_warga = pd.DataFrame(warga_response.data)
         df_pemeriksaan = pd.DataFrame(pemeriksaan_response.data) if pemeriksaan_response.data else pd.DataFrame()
         
-        
-        
         # Hitung Usia warga berdasarkan hari ini
         df_warga['tanggal_lahir'] = pd.to_datetime(df_warga['tanggal_lahir'])
-        df_warga['usia'] = (pd.to_datetime(df_pemeriksaan['tanggal_pemeriksaan']).dt.date - df_warga['tanggal_lahir']).dt.days / 365.25
+        df_warga['usia'] = (df_pemeriksaan['tanggal_pemeriksaan'] - df_warga['tanggal_lahir']).dt.days / 365.25
 
         # --- Filter di Halaman Utama ---
         st.subheader("Filter Laporan")
